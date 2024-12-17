4248f405cb6cfe79019681688f268cb47fb330f1
gjpgejx <gjpgejx13@gmail.com>
2024-12-17 16:23:53 +0300

    upd token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "7931207148:AAHlb_A0fPOhM-xWytxwOm1bxfohE2P-vEM"
+TOKEN = "7931207148:AAFoy93S7unY8V8iz95KU2ckA5JLNVFLimU"
 TEMP_DIR = "temp/"
 SUPPORTED_DOCS = ['docx', 'odt', 'txt']
 SUPPORTED_IMAGES = ['jpg', 'jpeg', 'png', 'bmp']
